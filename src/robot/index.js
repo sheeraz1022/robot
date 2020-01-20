7064924a1c46db3533a328189a969fdaec369b6d
Sheeraaz Shaikh <sheeraz.shaikh@snappymob.com>
2020-01-20 23:14:39 +0800

    - Added lodash package in dependencies - Created Robot which can be placed on table on safe coordinates

---
+++ src/robot/index.js
@@ -0,0 +1,76 @@
+import _ from "lodash";
+import Command from "../command/command";
+import Place from "../command/place"
+
+
+export default class Robot {
+  /**
+   * Construct a Robot within the boundaries of the table
+   * @param {Table} table
+   */
+  constructor(table) {
+    this.table = table;
+    this.robot = null;
+  }
+
+  /**
+   * Hang around within the boundaries of the table
+   * @param {Command} command
+   */
+  _safeToHangAround(intentedCommand) {
+    let tableEdges = this.table.Edges;
+    return (
+      intentedCommand.X <= tableEdges.x2 &&
+      intentedCommand.X >= tableEdges.x1 &&
+      intentedCommand.Y <= tableEdges.y2 &&
+      intentedCommand.Y >= tableEdges.y1
+    );
+  }
+
+  /**
+   * Updates intended state of Robot
+   * @param {Command} command
+   */
+  _changeRobotState(command) {
+    this.robot = { position: command };
+    return true;
+  }
+
+  /**
+   * Execute the command to change Robot's position or Orientation within boundaries
+   * @param {Command} command
+   */
+  _execute(command) {
+    if (!_.isEmpty(this.robot)) {
+      let commandResult = command.Execute(this.robot.position);
+      if (this._safeToHangAround(commandResult)) {
+        return this._changeRobotState(commandResult);
+      }
+    }
+    return false;
+  }
+
+
+  /**
+   * To place the Robot on the table within boundaries
+   */
+  PlaceRobot(x, y, direction) {
+    let result = Place.Execute(new Command(x, y, direction));
+
+    if (this._safeToHangAround(result)) {
+      return this._changeRobotState(result);
+    }
+
+    return false;
+  }
+
+  /**
+   * To get the current position of the Robot
+   */
+  GetRobotPosition() {
+    if (!_.isEmpty(this.robot)) {
+      return this.robot.position;
+    }
+    return null;
+  }
+}
---
+++ src/utils/direction.js
@@ -0,0 +1,11 @@
+/**
+ * Export an object for directions
+ */
+
+export default {
+    NORTH: "NORTH",
+    SOUTH: "SOUTH",
+    EAST: "EAST",
+    WEST: "WEST"
+  };
+  
