047ac62365a8db7f9f0a191a225879279a976488
sheeraz1022 <sheeraz.petarian@gmail.com>
2020-01-21 22:46:05 +0500

    Completes table and robot specifications

---
+++ tests/specs/movement.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import chai from "chai";
+const expect = chai.expect;
+
+export default () => {
+    describe("Checks robot to move in the appropriate directions", () => {
+        it("should just run and remain silent", () => {
+            return expect(true).to.be.true;
+        });
+    });
+};
